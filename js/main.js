999906de0077f3d0e56b315e07a96a376bb6ba0f
Dustin Smith <thedustinsmith@gmail.com>
2015-02-27 09:23:35 -0600

    Add a field array and generate a form

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
 (function () {
+	"use strict";
 
-	log("herasdfe");
-	$(function() {
+	var requiredFields = [
+		'url',
+		'key',
+		'secret',
+		'resourceLinkId',
+		'resourceLinkTitle',
+		'resourceLinkDescription',
+		'userId',
+		'roles',
+		'sourcedId',
+		'contextId',
+		'contextTitle',
+		'contextLabel'
+	];
 
-	});
+	var template = $("#form-field-template").html();
+	var fieldHtml = requiredFields.map(function (f) { // uses native array map.  Won't work in new browsers
+		return Mustache.render(template, {field: f });
+	}).join('');
+
+	$("#form").prepend(fieldHtml);
 })();
