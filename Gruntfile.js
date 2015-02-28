f2b344647fbd967e9bcca81841329de2c4305a4e
Dustin Smith <thedustinsmith@gmail.com>
2015-02-27 21:38:12 -0600

    some changes for gh-pages

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,6 +9,13 @@ module.exports = function (grunt) {
 					port: 8000,
 					base: './'
 				}
+			},
+			dist: {
+				options: {
+					port: 8000,
+					base: './dist/',
+					keepalive: true
+				}
 			}
 		},
 
@@ -20,26 +27,71 @@ module.exports = function (grunt) {
 				files: {
 					'./css/all.min.css': ['./css/main.less']
 				}
+			},
+			dist: {
+				options: {
+					cleancss: true
+				},
+				files: {
+					'./dist/css/all.min.css': ['./css/main.less']
+				}
+			}
+		},
+
+		copy: {
+			dist: {
+				files: [
+					{expand: true, flatten: true, src:'*.html', dest:'./dist/', filter: 'isFile' }
+				]
+			}
+		},
+
+		useminPrepare: {
+			html: 'dist/**/*.html',
+			options: {
+				dest: 'dist',
+				root: './'
+			}
+		},
+		usemin: {
+			html: ['dist/{,*/}*.html'],
+			// css: ['dist/css/{,*/}*.css'],
+			options: {
+				dirs: ['dist']
 			}
 		},
 
 		watch: {
 			evrthing: {
 				files: ['./css/*.less'], //, './js/*.js'],
-				tasks: ['less']
+				tasks: ['less:development']
 			},
 		}, 
-
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-connect');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-usemin');
 	grunt.loadNpmTasks('grunt-newer');
 
 	/* grunt tasks */
 	grunt.registerTask('default', [
-		'less', 
-		'connect', 
+		'less:development', 
+		'connect:dev', 
 		'watch']);
+
+	grunt.registerTask('prod', [
+		'copy',
+		'useminPrepare', 
+		'concat:generated',
+		'uglify:generated',
+		'less:dist',
+		'usemin', 
+		'connect:dist'
+		]);
 };
